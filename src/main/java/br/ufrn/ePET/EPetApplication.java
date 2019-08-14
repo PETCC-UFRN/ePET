68fd370663cd87ccc31f56acedf312dcfb3ff6d1
confuzo <abraaovld@gmail.com>
2019-08-14 18:59:35 -0300

    updating domain

---
+++ src/main/java/br/ufrn/ePET/EPetApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.ePET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/ePET/EPetApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufrn.ePET;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EPetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
