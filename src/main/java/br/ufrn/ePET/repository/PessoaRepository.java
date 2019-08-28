a7133f982413762883d21bdc1b11037ac38d9a72
confuzo <abraaovld@gmail.com>
2019-08-28 00:03:51 -0300

    repository

---
+++ src/main/java/br/ufrn/ePET/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package br.ufrn.ePET.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufrn.ePET.models.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+
+}
