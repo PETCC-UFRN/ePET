5fab6451879ebdfc75a5b63a3a0f7de9f5717797
confuzo <abraaovld@gmail.com>
2019-09-03 23:42:23 -0300

    implemented method findByNome

--- src/main/java/br/ufrn/ePET/repository/PessoaRepository.java
+++ src/main/java/br/ufrn/ePET/repository/PessoaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.ufrn.ePET.models.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	Pessoa findByNome(String nome);
 }
