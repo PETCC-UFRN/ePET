d1e390780d0e02c788685bb5dc0ca364b296c544
confuzo <abraaovld@gmail.com>
2019-09-07 00:40:46 -0300

    changed for httpBasic

--- src/main/java/br/ufrn/ePET/config/SecurityConfig.java
+++ src/main/java/br/ufrn/ePET/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.anyRequest().authenticated()
-		.and().formLogin()
+		.and().httpBasic()
 		.and().csrf().disable()
 		.logout().logoutSuccessUrl("/logout");
 	}
