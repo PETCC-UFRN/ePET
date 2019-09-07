c452a1b0a7353c08bf98dc9ab01fb24108205a3c
confuzo <abraaovld@gmail.com>
2019-09-07 00:43:34 -0300

    extra function

--- src/main/java/br/ufrn/ePET/repository/Tipo_UsuarioRepository.java
+++ src/main/java/br/ufrn/ePET/repository/Tipo_UsuarioRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.ufrn.ePET.models.Tipo_Usuario;
 
 public interface Tipo_UsuarioRepository extends JpaRepository<Tipo_Usuario, Long>{
-
+	
+	Tipo_Usuario findByNome(String nome);
 }
