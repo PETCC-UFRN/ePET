bd7a89d19a0dc3a254a1f80bc54b0698500308cc
confuzo <abraaovld@gmail.com>
2019-09-07 00:42:37 -0300

    service pessoa

--- src/main/java/br/ufrn/ePET/service/PessoaService.java
+++ src/main/java/br/ufrn/ePET/service/PessoaService.java
@@ -2,7 +2,6 @@ package br.ufrn.ePET.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import br.ufrn.ePET.models.Pessoa;
 import br.ufrn.ePET.repository.PessoaRepository;
@@ -21,31 +20,16 @@ public class PessoaService {
 		this.tipo_UsuarioRespository = tipo_UsuarioRespository;
 	}
 	
-	//@Secured("ROLE_petiano")
 	public Pessoa buscar(Long id) {
-		try {
-			return pessoaRepository.findById(id).get();
-		} catch (Exception e) {
-			return null;
-		}
+		return pessoaRepository.findById(id).get();
 	}
 	
-	//@Secured("ROLE_tutor")
 	public List<Pessoa> buscar(){
-		try {
-			return pessoaRepository.findAll();
-		} catch (Exception e) {
-			return null;
-		}
+		return pessoaRepository.findAll();
 	}
 	
 	public Pessoa salvar(Long id, Pessoa pessoa){
-		try {
-			pessoa.setTipo_usuario(tipo_UsuarioRespository.findById(id).get());
-			return pessoaRepository.save(pessoa);
-		} catch (Exception e) {
-			return null;
-		}
+		pessoa.setTipo_usuario(tipo_UsuarioRespository.findById(id).get());
+		return pessoaRepository.save(pessoa);
 	}
-	
 }
