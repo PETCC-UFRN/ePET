c81e242573d078cb127c0e26af48b49533150265
confuzo <abraaovld@gmail.com>
2019-09-07 00:42:49 -0300

    service petiano

--- src/main/java/br/ufrn/ePET/service/PetianoService.java
+++ src/main/java/br/ufrn/ePET/service/PetianoService.java
@@ -2,84 +2,76 @@ package br.ufrn.ePET.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.ufrn.ePET.models.Pessoa;
 import br.ufrn.ePET.models.Petiano;
 import br.ufrn.ePET.repository.PessoaRepository;
 import br.ufrn.ePET.repository.PetianoRepository;
+import br.ufrn.ePET.repository.Tipo_UsuarioRepository;
 
 @Service
 public class PetianoService {
 
 	private final PetianoRepository petianoRepository;
 	private final PessoaRepository pessoaRepository;
-	private final PessoaService pessoaservice;
-
+	private final Tipo_UsuarioRepository tipoUsuarioRepository;
 	@Autowired
-	public PetianoService(PetianoRepository petianoRepository, PessoaRepository pessoaRepository, PessoaService pessoaservice) {
+	public PetianoService(PetianoRepository petianoRepository, PessoaRepository pessoaRepository, Tipo_UsuarioRepository tipoUsuarioRepository) {
 		this.petianoRepository = petianoRepository;
 		this.pessoaRepository = pessoaRepository;
-		this.pessoaservice = pessoaservice;
+		this.tipoUsuarioRepository = tipoUsuarioRepository;
 	}
 
 	
 	public Petiano buscar(Long id) {
-		Petiano petiano = petianoRepository.findById(id).get();
-		return petiano instanceof Petiano ? petiano : null;
+		return petianoRepository.findById(id).get();
 	}
 	
-	public List<Petiano> buscar(){
+	public List<Petiano> buscarAtuais(){
 		List<Petiano> listPetianos = petianoRepository.findAll();
 		List<Petiano> listPetianosativos = new ArrayList<Petiano>();
 		for(Petiano petiano : listPetianos) {
-			if (petiano.getPessoa().getTipo_usuario().getNome() != "petiano") {
+			if (petiano.getPessoa().getTipo_usuario().getNome().equalsIgnoreCase("petiano")) {
 				listPetianosativos.add(petiano);
 			}
 		}
-		return petianoRepository.findAll();
+		return listPetianosativos;
+	}
+	
+	public List<Petiano> buscarAntigos(){
+		List<Petiano> listPetianos = petianoRepository.findAll();
+		for(Petiano petiano : listPetianos) {
+			if (!petiano.getPessoa().getTipo_usuario().getNome().equalsIgnoreCase("petiano")) {
+				listPetianos.remove(petiano);
+			}
+		}
+		return listPetianos;
 	}
 	
 	public Petiano salvar(Long id, Petiano petiano){
 		Pessoa pessoa = pessoaRepository.findById(id).get();
-		/*
-		List<Petiano> listPetianos = petianoRepository.findAll();
-		for (Petiano pet : listPetianos) {
-			if (pet.getPessoa().equals(pessoa)) {
-				petiano = pet;
-			}
-			
-		}*/
-		pessoa = pessoaservice.salvar(Long.valueOf(1), pessoa);
-		petiano.setPessoa(pessoa);	
+		pessoa.setTipo_usuario(tipoUsuarioRepository.findByNome("petiano"));
+		petiano.setPessoa(pessoaRepository.save(pessoa));	
 		return petianoRepository.save(petiano);
 		
 	}
+	
 	
 	public Petiano remover(Long id){
 		Petiano petiano = petianoRepository.findById(id).get();
 		Pessoa pessoa = petiano.getPessoa();
-		pessoa = pessoaservice.salvar(Long.valueOf(3), pessoa);
-		petiano.setPessoa(pessoa);
+		pessoa.setTipo_usuario(tipoUsuarioRepository.findByNome("comum"));
+		petiano.setPessoa(pessoaRepository.save(pessoa));
 		petiano.setData_egresso(LocalDate.now());
-
-		return petianoRepository.save(petiano);
-		
+		return petianoRepository.save(petiano);	
 	}
 	
 	public Petiano editar(Long id, Petiano petiano){
-		Petiano petianoAntigo = petianoRepository.findById(id).get();
-		Pessoa pessoa = petianoAntigo.getPessoa();
-		petiano.setPessoa(pessoa);
-		petiano.setIdPetiano(petianoAntigo.getIdPetiano());
-
+		petiano.setPessoa(pessoaRepository.findById(id).get());
 		return petianoRepository.save(petiano);
-		
 	}
 }
