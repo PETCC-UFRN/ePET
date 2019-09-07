0fb2d4ab3ee97f6b511aa2090a92ff6920c48fb0
confuzo <abraaovld@gmail.com>
2019-09-07 00:41:30 -0300

    controller petiano

--- src/main/java/br/ufrn/ePET/controller/PetianoController.java
+++ src/main/java/br/ufrn/ePET/controller/PetianoController.java
@@ -1,13 +1,10 @@
 package br.ufrn.ePET.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-/*import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;*/
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.ePET.models.Petiano;
-import br.ufrn.ePET.repository.PetianoRepository;
 import br.ufrn.ePET.service.PetianoService;
 
 @RestController
@@ -30,34 +26,65 @@ public class PetianoController {
 		this.petianoservice = petianoservice;
 	}
 
-	@GetMapping(value="/petianos")
-	//@PreAuthorize("hasAnyRole('ADMIN', 'TUTOR')")
-	public List<Petiano> getPetianos(){
-		return petianoservice.buscar();
+	@GetMapping(value="/petianos-atuais")
+	public ResponseEntity<?> getPetianosAtuais(){
+		try {
+			return new ResponseEntity<>(petianoservice.buscarAtuais(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@GetMapping(value="/petianos-antigos")
+	public ResponseEntity<?> getPetianosAntigos(){
+		try {
+			return new ResponseEntity<>(petianoservice.buscarAntigos(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
 	}
 
 	@GetMapping(value="/petianos/{id}")
+	@Secured({"ROLE_tutor", "ROLE_petiano"})
 	public Petiano getPetianos(@PathVariable Long id){
 		return petianoservice.buscar(id);
 	}
 
 	@PostMapping(value="/petianos-cadastro/{id}")
+	@Secured("ROLE_tutor")
 	public ResponseEntity<?> savePetianos(@PathVariable Long id, @RequestBody Petiano petiano){
-		petianoservice.salvar(id, petiano);
-		return new ResponseEntity<>(HttpStatus.OK);
+		try{
+			petianoservice.salvar(id, petiano);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
-	@PostMapping(value="/petianos-remove/{id}")
+	@DeleteMapping(value="/petianos-remove/{id}")
+	@Secured("ROLE_tutor")
 	public ResponseEntity<?> removePetianos(@PathVariable Long id){
-		petianoservice.remover(id);
-		return new ResponseEntity<>(HttpStatus.OK);
+		try {
+			petianoservice.remover(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 
 	@PostMapping(value="/petianos-editar/{id}")
+	@Secured({"ROLE_tutor", "ROLE_petiano"})
 	public ResponseEntity<?> editaPetianos(@PathVariable Long id, @RequestBody Petiano petiano){
-		petianoservice.editar(id, petiano);
-		return new ResponseEntity<>(HttpStatus.OK);
+		try{
+			petianoservice.editar(id, petiano);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
 	}
 	
 }
