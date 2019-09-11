70d7a18d00410f92da0d1419d340004abbbc744a
confuzo <abraaovld@gmail.com>
2019-09-11 09:20:48 -0300

    adding permissions to Evento controller

--- src/main/java/br/ufrn/ePET/controller/EventoController.java
+++ src/main/java/br/ufrn/ePET/controller/EventoController.java
@@ -2,12 +2,14 @@ package br.ufrn.ePET.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.ePET.models.Evento;
@@ -53,7 +55,19 @@ public class EventoController {
 		}
 	}
 	
+	@PostMapping(value = "/eventos-ativar/{id}")
+	@Secured("ROLE_tutor")
+	public ResponseEntity<?> ativarEventos(@RequestParam Long id){
+		try {
+			eventoService.ativar(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
 	@PostMapping(value = "/eventos-cadastrar")
+	@Secured({"ROLE_tutor", "ROLE_petiano"})
 	public ResponseEntity<?> saveEventos(@RequestBody Evento evento){
 		try {
 			eventoService.salvar(evento);
@@ -64,6 +78,7 @@ public class EventoController {
 	}
 	
 	@DeleteMapping(value="/eventos-remove/{id}")
+	@Secured({"ROLE_tutor", "ROLE_petiano"})
 	public ResponseEntity<?> removeEventos(@PathVariable Long id){
 		try {
 			eventoService.remover(id);;
