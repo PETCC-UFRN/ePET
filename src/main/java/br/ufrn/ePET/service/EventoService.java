93292750200270989e42c30b7e075dc755b0e809
confuzo <abraaovld@gmail.com>
2019-09-10 23:11:36 -0300

    Evento service implemented

---
+++ src/main/java/br/ufrn/ePET/service/EventoService.java
@@ -0,0 +1,48 @@
+package br.ufrn.ePET.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.ufrn.ePET.models.Evento;
+import br.ufrn.ePET.repository.EventoRepository;
+
+@Service
+public class EventoService {
+	
+	private final EventoRepository eventoRepository;
+	
+	@Autowired
+	public EventoService(EventoRepository eventoRepository) {
+		this.eventoRepository = eventoRepository;
+	}
+	
+	public Evento buscar(Long id) {
+		return eventoRepository.findById(id).get();
+	}
+	
+	public List<Evento> buscar(){
+		return eventoRepository.findAll();
+	}
+	
+	public List<Evento> buscarAtivos(){
+		List<Evento> lista = eventoRepository.findAll();
+		LocalDate ld = LocalDate.now();
+		for(Evento e : lista) {
+			if(!(ld.compareTo(e.getD_inscricao()) >= 0) && !(ld.compareTo(e.getD_inscricao_fim()) >= 0)) {
+				lista.remove(e);
+			}
+		}
+		return lista;
+	}
+	
+	public Evento salvar(Evento evento) {
+		return eventoRepository.save(evento);
+	}
+	
+	public void remover(Long id) {
+		eventoRepository.deleteById(id);
+	}
+}
