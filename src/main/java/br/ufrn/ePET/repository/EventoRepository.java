9b8decdea64841cd446c370710d2ecf79862ceee
confuzo <abraaovld@gmail.com>
2019-09-10 23:11:23 -0300

    Evento repository implemented

---
+++ src/main/java/br/ufrn/ePET/repository/EventoRepository.java
@@ -0,0 +1,12 @@
+package br.ufrn.ePET.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.ufrn.ePET.models.Evento;
+
+public interface EventoRepository extends JpaRepository<Evento, Long>{
+	
+}
