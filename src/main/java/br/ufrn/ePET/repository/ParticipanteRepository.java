a6f2bf6737dd790bfe4b32584431f38cf60e3068
confuzo <abraaovld@gmail.com>
2019-09-13 10:03:10 -0300

    Participante repository in progress

--- src/main/java/br/ufrn/ePET/repository/ParticipanteRepository.java
+++ src/main/java/br/ufrn/ePET/repository/ParticipanteRepository.java
@@ -1,9 +1,19 @@
 package br.ufrn.ePET.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.ufrn.ePET.models.Participante;
 
 public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
-
+	
+	@Query(value = "SELECT count(id_participante) FROM `participante` WHERE id_evento = ?1", nativeQuery = true)
+	int countAtivos(Long id);
+	
+	List<Participante> findByEspera(boolean espera);
+	
+	@Query(value = "SELECT * FROM PARTICIPANTES u WHERE u.id_pessoa = ?1", nativeQuery = true)
+	List<Participante> findByPessoa(Long id);
 }
