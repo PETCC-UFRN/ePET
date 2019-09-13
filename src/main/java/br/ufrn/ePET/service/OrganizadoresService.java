8989fd60277477ad9f2de364632ec5cf3a4c906b
confuzo <abraaovld@gmail.com>
2019-09-13 10:00:18 -0300

    Organizadores Service implemented

--- src/main/java/br/ufrn/ePET/service/OrganizadoresService.java
+++ src/main/java/br/ufrn/ePET/service/OrganizadoresService.java
@@ -4,30 +4,41 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.ufrn.ePET.models.Organizadores;
+import br.ufrn.ePET.repository.EventoRepository;
 import br.ufrn.ePET.repository.OrganizadoresRepository;
+import br.ufrn.ePET.repository.PessoaRepository;
 
 @Service
 public class OrganizadoresService {
 	
 	private final OrganizadoresRepository organizadoresRepository;
+	private final PessoaRepository pessoaRepository;
+	private final EventoRepository eventoRepository;
 	
 	@Autowired
-	public OrganizadoresService(OrganizadoresRepository organizadoresRepository) {
+	public OrganizadoresService(OrganizadoresRepository organizadoresRepository, PessoaRepository pessoaRepository,
+			EventoRepository eventoRepository) {
 		this.organizadoresRepository = organizadoresRepository;
+		this.pessoaRepository = pessoaRepository;
+		this.eventoRepository = eventoRepository;
 	}
 	
 	public List<Organizadores> buscar(){
 		return organizadoresRepository.findAll();
 	}
-	
+
 	public Organizadores buscar(Long id) {
 		return organizadoresRepository.findById(id).get();
 	}
 	
-	public void salvar(Organizadores organizadores) {
-		organizadoresRepository.save(organizadores);
+	public void salvar(Long id_evento, Long id_pessoa) {
+		Organizadores o = new Organizadores();
+		o.setEvento(eventoRepository.findById(id_evento).get());
+		o.setPessoa(pessoaRepository.findById(id_pessoa).get());
+		organizadoresRepository.save(o);
 	}
 	
 	public List<Organizadores> buscarPessoa(Long id){
