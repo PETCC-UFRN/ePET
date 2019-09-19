8caf604f0fb790b1f3a2e2658efcb2fc892966f6
confuzo <abraaovld@gmail.com>
2019-09-19 15:25:04 -0300

    Periodo Evento repository

---
+++ src/main/java/br/ufrn/ePET/repository/Periodo_EventoReposioty.java
@@ -0,0 +1,9 @@
+package br.ufrn.ePET.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufrn.ePET.models.Periodo_Evento;
+
+public interface Periodo_EventoReposioty extends JpaRepository<Periodo_Evento, Long>{
+
+}
