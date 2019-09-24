f616d8f4eecd978abad630c8d1a4d74786f554db
confuzo <abraaovld@gmail.com>
2019-09-24 15:29:09 -0300

    New error handler

---
+++ src/main/java/br/ufrn/ePET/error/DuplicatedEntryException.java
@@ -0,0 +1,14 @@
+package br.ufrn.ePET.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DuplicatedEntryException extends RuntimeException{
+	
+	public DuplicatedEntryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
