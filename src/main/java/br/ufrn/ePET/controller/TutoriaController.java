e0423b8aa5ea9621e253a9edcb753868254f123c
Danhfg <danielhenriquefg@gmail.com>
2019-10-07 22:42:56 -0300

    Update TutoriaController.java

--- src/main/java/br/ufrn/ePET/controller/TutoriaController.java
+++ src/main/java/br/ufrn/ePET/controller/TutoriaController.java
@@ -53,7 +53,7 @@ public class TutoriaController {
 	
 	@PostMapping(value="/tutoria-cadastro/{id_petiano}/{id_disciplina}")
 	@Secured({"ROLE_tutor", "ROLE_petiano"})
-	public ResponseEntity<?> saveTutoria(@PathVariable Long id_petiano, @PathVariable Long id_disciplina,){
+	public ResponseEntity<?> saveTutoria(@PathVariable Long id_petiano, @PathVariable Long id_disciplina){
 		Tutoria tutoria = new Tutoria();
 		tutoriaService.salvar(id_petiano, id_disciplina, tutoria);
 		return new ResponseEntity<>(HttpStatus.OK);
