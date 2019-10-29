0f2bf890a4c9643a60dcdae40eac662da8b706f1
confuzo <abraaovld@gmail.com>
2019-10-29 00:49:51 -0300

    updated anexo noticia controller

--- src/main/java/br/ufrn/ePET/controller/Anexo_NoticiaController.java
+++ src/main/java/br/ufrn/ePET/controller/Anexo_NoticiaController.java
@@ -14,21 +14,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.ufrn.ePET.error.ResourceNotFoundException;
 import br.ufrn.ePET.models.Anexo_Noticia;
 import br.ufrn.ePET.service.Anexo_NoticiaService;
+import br.ufrn.ePET.service.FileStorageService;
 
 @RestController
 @RequestMapping(value = "/api")
 public class Anexo_NoticiaController {
 	
 	private final Anexo_NoticiaService anexo_NoticiaService;
+	private final FileStorageService fileStorageService;
 	
 	@Autowired
-	public Anexo_NoticiaController(Anexo_NoticiaService anexo_NoticiaService) {
+	public Anexo_NoticiaController(Anexo_NoticiaService anexo_NoticiaService, FileStorageService fileStorageService) {
 		this.anexo_NoticiaService = anexo_NoticiaService;
+		this.fileStorageService = fileStorageService;
 	}
 	
 	@GetMapping(value = "/anexos-noticias/{id}")
@@ -46,6 +51,21 @@ public class Anexo_NoticiaController {
 		anexo_NoticiaService.salvar(id_noticia, anexo_Noticia);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	
+	@PostMapping(value = "/anexos-noticia-upload/{id_noticia}")
+	@Secured({"ROLE_tutor", "ROLE_petiano"})
+	public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable Long id_noticia) {
+	     String filename = fileStorageService.storeFile(file);
+	     if(filename != null) {
+	    	 Anexo_Noticia anexo_Noticia = new Anexo_Noticia();
+	    	 anexo_Noticia.setAnexos(filename);
+	    	 anexo_NoticiaService.salvar(id_noticia, anexo_Noticia);
+	    	 return new ResponseEntity<>(HttpStatus.OK);
+	     } else {
+	    	 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	     }
+	}
+	
 	
 	@DeleteMapping(value = "/anexos-noticia-remove/{id}")
 	@Secured({"ROLE_tutor", "ROLE_petiano"})
