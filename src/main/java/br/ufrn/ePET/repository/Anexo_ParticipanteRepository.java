7c9b092abd856c42776577b76b89cafb9d134614
confuzo <abraaovld@gmail.com>
2019-10-29 00:48:10 -0300

    create anexo participante repository

---
+++ src/main/java/br/ufrn/ePET/repository/Anexo_ParticipanteRepository.java
@@ -0,0 +1,17 @@
+package br.ufrn.ePET.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.ufrn.ePET.models.Anexo_Participante;
+
+public interface Anexo_ParticipanteRepository extends JpaRepository<Anexo_Participante, Long>{
+	
+	@Query(value = "SELECT * FROM anexo_participante a WHERE a.id_participante = ?1", nativeQuery = true)
+	List<Anexo_Participante> findByIdParticipante(Long id_participante);
+	
+	@Query(value = "DELETE FROM anexo_participante a WHERE a.id_participante = ?1", nativeQuery = true)
+	void deleteByIdParticipante(Long id_participante);
+}
