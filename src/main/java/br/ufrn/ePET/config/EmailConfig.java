c89154a06aa591fff7e165078b489925949bb440
Confuzo <abraaovld@gmail.com>
2019-12-13 02:21:05 -0300

    started email implementation

---
+++ src/main/java/br/ufrn/ePET/config/EmailConfig.java
@@ -0,0 +1,20 @@
+package br.ufrn.ePET.config;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailConfig {
+	private JavaMailSender javaMailSender;
+	
+	public void enviarEmail(String email_petiano, String email_requisitante, String tutoria, String petiano) {
+		SimpleMailMessage smm = new SimpleMailMessage();
+		smm.setTo(email_petiano);
+		smm.setText("Olá " + petiano + "!\n"
+				+ "Tem uma nova solicitação de tutoria da disciplina de " + tutoria + " feita por " + email_requisitante);
+		try {
+			javaMailSender.send(smm);
+		} catch (Exception e) {
+			throw new RuntimeException("Houve algum erro no envio do seu email!\n" + e);
+		}
+	}
+}
