aaa641cfe0bb072baae9c584756327747205f7cc
Confuzo <abraaovld@gmail.com>
2019-12-13 02:24:24 -0300

    general updates

--- src/main/java/br/ufrn/ePET/EPetApplication.java
+++ src/main/java/br/ufrn/ePET/EPetApplication.java
@@ -3,6 +3,7 @@ package br.ufrn.ePET;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.ufrn.ePET.config.FileStorageConfig;
 
--- src/main/java/br/ufrn/ePET/encript.java
+++ src/main/java/br/ufrn/ePET/encript.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class encript {
 	public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("123456789"));
+		System.out.println(new BCryptPasswordEncoder().encode("password"));
 		System.out.println(LocalDate.now());
 	}
 }
