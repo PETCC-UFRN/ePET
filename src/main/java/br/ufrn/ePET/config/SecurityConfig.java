93e500646000292f1ca6b07d9821407ecbd99b21
Confuzo <abraaovld@gmail.com>
2020-02-17 00:16:50 -0300

    Updating security

--- src/main/java/br/ufrn/ePET/config/SecurityConfig.java
+++ src/main/java/br/ufrn/ePET/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package br.ufrn.ePET.config;
 
+import br.ufrn.ePET.security.JWTAutheticationFilter;
+import br.ufrn.ePET.security.JWTAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +16,8 @@ import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 import br.ufrn.ePET.service.CustomUserDetailsService;
 
+import static br.ufrn.ePET.security.SecurityConstants.SIGN_UP_URL;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -21,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private  CustomUserDetailsService userDetailsService;
 	
-	@Override
+	/*@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.anyRequest().authenticated()
@@ -29,8 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().csrf().disable()
 		.addFilterBefore(new CORSConfig(), ChannelProcessingFilter.class)
 		.logout().logoutSuccessUrl("/logout");
+	}*/
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 http.cors().and().csrf().disable().authorizeRequests()
+				 .antMatchers(HttpMethod.GET, SIGN_UP_URL).permitAll()
+				 .and()
+				 .formLogin()
+				 .and()
+				 .addFilter(new JWTAutheticationFilter(authenticationManager()))
+				 .addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService));
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
--- src/main/java/br/ufrn/ePET/security/SecurityConstants.java
+++ src/main/java/br/ufrn/ePET/security/SecurityConstants.java
@@ -1,4 +1,13 @@
 package br.ufrn.ePET.security;
 
-public enum SecurityConstants {
+import java.util.concurrent.TimeUnit;
+
+public class SecurityConstants {
+
+    static final String SECRET = "";
+    static final String TOKEN_PREFIX = "Bearer";
+    static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/api/sign-up";
+    static final Long EXPIRATION_TIME = 10800000L;
+
 }
