bab3f21ea046e74eb063d88aa7709357bd080141
Confuzo <abraaovld@gmail.com>
2020-02-27 22:17:00 -0300

    Implemented constants

--- src/main/java/br/ufrn/ePET/security/SecurityConstants.java
+++ src/main/java/br/ufrn/ePET/security/SecurityConstants.java
@@ -7,7 +7,7 @@ public class SecurityConstants {
     static final String SECRET = "";
     static final String TOKEN_PREFIX = "Bearer";
     static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/sign-up";
+    public static final String SIGN_UP_URL = "/api/sign-in/";
     static final Long EXPIRATION_TIME = 10800000L;
 
 }
