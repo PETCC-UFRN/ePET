68ae0c32cef8b8350798fee46290a3f4b5418a59
Confuzo <abraaovld@gmail.com>
2020-02-27 22:15:23 -0300

    Updating WebSecurityConfig class

--- src/main/java/br/ufrn/ePET/config/SecurityConfig.java
+++ src/main/java/br/ufrn/ePET/config/SecurityConfig.java
@@ -2,19 +2,26 @@ package br.ufrn.ePET.config;
 
 import br.ufrn.ePET.security.JWTAutheticationFilter;
 import br.ufrn.ePET.security.JWTAuthorizationFilter;
+import br.ufrn.ePET.security.JwtTokenFilterConfigurer;
+import br.ufrn.ePET.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 import br.ufrn.ePET.service.CustomUserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
 
 import static br.ufrn.ePET.security.SecurityConstants.SIGN_UP_URL;
 
@@ -23,9 +30,11 @@ import static br.ufrn.ePET.security.SecurityConstants.SIGN_UP_URL;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	/*@Autowired
+	private  CustomUserDetailsService userDetailsService;*/
+
 	@Autowired
-	private  CustomUserDetailsService userDetailsService;
-	
+	private JwtTokenProvider jwtTokenProvider;
 	/*@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -38,26 +47,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 http.cors().and().csrf().disable().authorizeRequests()
-				 .antMatchers(HttpMethod.GET, SIGN_UP_URL).permitAll()
-				 .and()
-				 .formLogin()
-				 .and()
-				 .addFilter(new JWTAutheticationFilter(authenticationManager()))
-				 .addFilter(new JWTAuthorizationFilter(authenticationManager(), userDetailsService));
+		 http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and().csrf().disable();
+
+		 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		 http.authorizeRequests()
+				 .antMatchers(SIGN_UP_URL).permitAll()
+				 .anyRequest().authenticated();
+
+		 //http.exceptionHandling().accessDeniedHandler("/login");
+		 http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
 	}
 
-	@Override
+	/*@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}*/
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/service/api/usuarios-cadastrar/**", "/service/swagger-ui.html#/**");
+		web.ignoring().antMatchers("/swagger-resources/**")//
+				.antMatchers("/swagger-ui.html");
 	}
 	
-	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 	
 	
 }
