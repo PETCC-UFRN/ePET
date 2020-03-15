5afdbdb264cf741fd960f92ae5f674d6c6da65c1
Confuzo <abraaovld@gmail.com>
2020-03-15 14:08:17 -0300

    Adding new constants

--- src/main/java/br/ufrn/ePET/security/SecurityConstants.java
+++ src/main/java/br/ufrn/ePET/security/SecurityConstants.java
@@ -4,10 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 public class SecurityConstants {
 
-    static final String SECRET = "";
+    static final String SECRET = "teste";
     static final String TOKEN_PREFIX = "Bearer";
     static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/sign-in/**";
+    public static final String SIGN_IN_URL = "/api/sign-in/**";
+    public static final String SIGN_UP_URL = "/api/sign-up/**";
     static final Long EXPIRATION_TIME = 10800000L;
 
 }
