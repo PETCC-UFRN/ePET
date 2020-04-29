ea7960f127c109fdb4b72afa4a063a19365ca5b5
Confuzo <abraaovld@gmail.com>
2020-04-29 01:44:27 -0300

    Fixing search by adding field

--- src/main/java/br/ufrn/ePET/repository/ParticipanteRepository.java
+++ src/main/java/br/ufrn/ePET/repository/ParticipanteRepository.java
@@ -23,13 +23,13 @@ public interface ParticipanteRepository extends JpaRepository<Participante, Long
 	@Query(value = "SELECT * FROM participante u WHERE u.id_pessoa = ?1 AND ORDER BY u.id_participante DESC", nativeQuery = true)
 	Page<Participante> findByPessoa(Long id, Pageable pageable);
 
-	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa FROM participante u INNER JOIN pessoa p ON p.id_pessoa = u.id_pessoa WHERE p.nome LIKE %?1% OR p.cpf LIKE %?1%", nativeQuery = true)
+	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa, u.data_maxima FROM participante u INNER JOIN pessoa p ON p.id_pessoa = u.id_pessoa WHERE p.nome LIKE %?1% OR p.cpf LIKE %?1%", nativeQuery = true)
 	Page<Participante> findByNomeOuCpfPessoa(String nome, Pageable pageable);
 
-	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa FROM participante u INNER JOIN evento e ON e.id_evento = u.id_evento WHERE e.titulo LIKE %?1%", nativeQuery = true)
+	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa, u.data_maxima FROM participante u INNER JOIN evento e ON e.id_evento = u.id_evento WHERE e.titulo LIKE %?1%", nativeQuery = true)
 	Page<Participante> findByTituloEvento(String titulo, Pageable pageable);
 
-	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa FROM participante u INNER JOIN evento e ON e.id_evento = u.id_evento WHERE e.titulo LIKE %?1%", nativeQuery = true)
+	@Query(value = "SELECT u.id_participante, u.confirmado, u.espera, u.id_evento, u.id_pessoa, u.data_maxima FROM participante u INNER JOIN evento e ON e.id_evento = u.id_evento WHERE e.titulo LIKE %?1%", nativeQuery = true)
 	List<Participante> findByTituloEvento(String titulo);
 
 	@Query(value = "SELECT * FROM participante u WHERE u.id_pessoa = ?1 AND u.id_evento = ?2", nativeQuery = true)
