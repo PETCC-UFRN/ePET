7dfd24ff868405323839d61572fff5cb2a70b637
Danhfg <danielhenriquefg@gmail.com>
2020-06-01 15:44:55 -0300

    Fix #82
    Esta rota estava sem as permissões de TOKEN, retornando sempre 403

--- src/main/java/br/ufrn/ePET/config/SecurityConfig.java
+++ src/main/java/br/ufrn/ePET/config/SecurityConfig.java
@@ -78,6 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				 .antMatchers(HttpMethod.GET, "/api/petianos-antigos").permitAll()
 				 .antMatchers(HttpMethod.GET, "/api/eventos-abertos").permitAll()
 				 .antMatchers(HttpMethod.POST, "/api/pagseguro-notificacao/**").permitAll()
+				 .antMatchers(HttpMethod.GET, "/api/certificado/validar/{hash}").permitAll()
 				 .antMatchers("/api/redirecionamento-pagamento/**").permitAll()
 				 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
 				 .anyRequest().authenticated();
